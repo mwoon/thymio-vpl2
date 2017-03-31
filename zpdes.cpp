806e9aed6b8150b156d0c1eb224d09fed0f04ff6
mwoon <marie_woon@hotmail.com>
2017-03-31 18:24:52 +0200

    modifications to accommodate testing environment

---
+++ zpdes.cpp
@@ -0,0 +1,6 @@
+#include "zpdes.h"
+
+zpdes::zpdes(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ zpdes.h
@@ -0,0 +1,17 @@
+#ifndef ZPDES_H
+#define ZPDES_H
+
+#include <QObject>
+
+class zpdes : public QObject
+{
+    Q_OBJECT
+public:
+    explicit zpdes(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // ZPDES_H
